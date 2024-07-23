100e6f9a5079472fd50147812d2b188133d20c33
olga soldatenko <olga.a.soldatenko@gmail.com>
2024-07-23 11:48:57 +0300

    configurations

---
+++ configurations/constants.py
@@ -0,0 +1,12 @@
+class Constants:
+    fault_max = 100000000
+    fault_min = 0.00000001
+    fault_max_cat = 0.999
+    fault_min_cat = 0.001
+    convergence_limit = 0.00005
+
+
+OUTPUT_LIB = "output"
+CONFIGURATION_DIR = "configurations"
+BACKUP = "Backup"
+NEW = "New"
